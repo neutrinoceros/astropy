bd72a07bf4155d8095600e6c648bec883fc1df21
Clément Robert <cr52@protonmail.com>
2024-07-01 20:22:21 +0200

    ruff 0.5: manual refactors (following code review)

--- astropy/table/tests/test_groups.py
+++ astropy/table/tests/test_groups.py
@@ -635,9 +635,6 @@ def test_column_filter():
     Table groups filtering
     """
 
-    def all_positive(column):
-        return bool(np.all(column >= 0))
-
     # Negative value in 'a' column should not filter because it is a key col
     t = Table.read(
         [
@@ -655,7 +652,7 @@ def test_column_filter():
         format="ascii",
     )
     tg = t.group_by("a")
-    c2 = tg["c"].groups.filter(all_positive)
+    c2 = tg["c"].groups.filter(lambda column: np.all(column >= 0))
     assert len(c2.groups) == 3
     assert c2.groups[0].pformat() == [" c ", "---", "7.0", "5.0"]
     assert c2.groups[1].pformat() == [" c ", "---", "0.0"]
